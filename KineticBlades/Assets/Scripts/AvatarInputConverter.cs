103829a6dde264678ec4d9cced8d382e189ca05d
Dela <stevedelatorre@gmail.com>
2020-08-22 16:37:55 -0400

    Final IK + Player Embodiment
    mostly working, down to the details

--- KineticBlades/Assets/Scripts/AvatarInputConverter.cs
+++ KineticBlades/Assets/Scripts/AvatarInputConverter.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AvatarInputConverter : MonoBehaviour
 {
-    public bool handTrackingIsInstant = true;
+    protected bool handTrackingIsInstant = true;
 
     // Avatar Transforms
     public Transform mainAvatarTransform;
@@ -31,42 +31,29 @@ public class AvatarInputConverter : MonoBehaviour
     {
         // Set all of the avatar game objects to active
         // just in case they were hidden for adjustments
-        avatarHead.gameObject.SetActive(true);
-        avatarBody.gameObject.SetActive(true);
-        avatarLeftHand.gameObject.SetActive(true);
-        avatarRightHand.gameObject.SetActive(true);
+        //avatarHead.gameObject.SetActive(true);
+        //avatarBody.gameObject.SetActive(true);
+        //avatarLeftHand.gameObject.SetActive(true);
+        //avatarRightHand.gameObject.SetActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
         // place the entire body where the center eye anchor is on OVRPlayerController
-        mainAvatarTransform.position = Vector3.Lerp(mainAvatarTransform.position, oculusHead.position + positionOffset, lerpSmoothing);
+        //mainAvatarTransform.position = Vector3.Lerp(mainAvatarTransform.position, oculusHead.position + positionOffset, lerpSmoothing);
         // The heads rotation will be updated to match CenterEyeAnchor
-        avatarHead.rotation = Quaternion.Lerp(avatarHead.rotation, oculusHead.rotation, lerpSmoothing);
+        //avatarHead.rotation = Quaternion.Lerp(avatarHead.rotation, oculusHead.rotation, lerpSmoothing);
 
         // Rotate the body just off the Y of the head
-        Vector3 avatarHeadRotY = new Vector3(0, avatarHead.rotation.eulerAngles.y, 0);
-        avatarBody.rotation = Quaternion.Lerp(avatarBody.rotation, Quaternion.Euler(avatarHeadRotY), (lerpSmoothing*2));
+        //Vector3 avatarHeadRotY = new Vector3(0, avatarHead.rotation.eulerAngles.y, 0);
+        //avatarBody.rotation = Quaternion.Lerp(avatarBody.rotation, Quaternion.Euler(avatarHeadRotY), (lerpSmoothing*2));
 
-        // If Hand tracking is not instant...
-        if (handTrackingIsInstant)
-        {
-            // We will update the avatar hands to match position....
-            avatarLeftHand.position = oculusLeftHand.position;
-            avatarRightHand.position = oculusRightHand.position;
-            // ...and then the rotation of hand anchors
-            avatarLeftHand.rotation = oculusLeftHand.rotation;
-            avatarRightHand.rotation = oculusRightHand.rotation;
-        } else
-        {
-            // We will update the avatar hands to match position....
-            avatarLeftHand.position = Vector3.Lerp(avatarLeftHand.position, oculusLeftHand.position, lerpSmoothing);
-            avatarRightHand.position = Vector3.Lerp(avatarRightHand.position, oculusRightHand.position, lerpSmoothing);
-            // ...and then the rotation of hand anchors
-            avatarLeftHand.rotation = Quaternion.Lerp(avatarLeftHand.rotation, oculusLeftHand.rotation, lerpSmoothing);
-            avatarRightHand.rotation = Quaternion.Lerp(avatarRightHand.rotation, oculusRightHand.rotation, lerpSmoothing);
-        }
+        avatarLeftHand.position = oculusLeftHand.position;
+        avatarRightHand.position = oculusRightHand.position;
+        // ...and then the rotation of hand anchors
+        avatarLeftHand.rotation = oculusLeftHand.rotation;
+        avatarRightHand.rotation = oculusRightHand.rotation;
         
         // The player can use their button on their right controller to change swords
         if (OVRInput.GetDown(OVRInput.Button.Two, OVRInput.Controller.RTouch))
